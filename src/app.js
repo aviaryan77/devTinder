ceebb0f061d3a25ca79fb3c353448c6b196e4501
AVINASH ARYAN <78313973+aviaryan77@users.noreply.github.com>
2025-03-03 07:24:07 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const { authRouter, requestRouter, profileRouter,userRouter } = require('./routes');
+const {
+  authRouter,
+  requestRouter,
+  profileRouter,
+  userRouter,
+} = require('./routes');
 
 // middleware to parse the incoming request body to JSON
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use('/', authRouter);
 app.use('/', requestRouter);
 app.use('/', profileRouter);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const UserModel = require('../models/user');
 
 const userRouter = express.Router();
 
-const USER_SAFE_DATA = 'firstName lastName photoUrl age skills about';  // safe data to be sent to the client
+const USER_SAFE_DATA = 'firstName lastName photoUrl age skills about'; // safe data to be sent to the client
 
 // get all the pending connection requests for the loggedInUser
 userRouter.get('/user/requests/received', userAuth, async (req, res) => {
@@ -33,18 +34,58 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         { fromUserId: loggedInUser?._id, status: 'accepted' },
         { toUserId: loggedInUser?._id, status: 'accepted' },
       ],
-    }).populate('fromUserId', USER_SAFE_DATA).populate('toUserId', USER_SAFE_DATA);
+    })
+      .populate('fromUserId', USER_SAFE_DATA)
+      .populate('toUserId', USER_SAFE_DATA);
 
     const data = requests.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser?._id.toString()) {
         return row.toUserId;
       }
-      return row.fromUserId
+      return row.fromUserId;
     });
 
     res.status(200).json({
       message: 'Connections fetched successfully',
       data: data,
+    });
+  } catch (error) {
+    res.status(500).send('something went wrong' + error);
+  }
+});
+
+// user feed API
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const requests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser?._id }, { toUserId: loggedInUser?._id }],
+    }).select('fromUserId toUserId');
+
+    const hideUsersFromFeedSet = new Set();
+    requests.forEach((row) => {
+      hideUsersFromFeedSet.add(row.fromUserId.toString());
+      hideUsersFromFeedSet.add(row.toUserId.toString());
+    });
+
+    hideUsersFromFeedSet.add(loggedInUser?._id.toString());
+
+    const users = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeedSet) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+    res.status(200).json({
+      message: 'Feed fetched successfully',
+      data: users,
     });
   } catch (error) {
     res.status(500).send('something went wrong' + error);
